0633ac01969f77149d8493b3294d9410a97ad8a0
AjiSetiawan1205 <47689775+AjiSetiawan1205@users.noreply.github.com>
2019-02-17 10:19:49 +0700

    Create license.sh

---
+++ license.sh
@@ -0,0 +1,83 @@
+#! / bin / bash
+
+
+bersih
+
+
+gema "
+
+<- DSFS Indo Team ->
+
+####################
+
+# By Xenon1205
+
+# D4RK SYST3M F41LUR3 S33K3R Team
+
+####################
+
+Semoga Berhasil " ;
+
+
+gema " " ;
+
+
+baca -p " Server: http: // " server
+
+
+baca p " File Log: " File
+
+
+baca -p " Delay (2-5): " delay
+
+
+gema " " ;
+
+
+echo " [+] Mengumpulkan Informasi Bro H3h3 ... " ;
+
+
+tidur 3 ;
+
+
+echo " [+] Server: http: // $ server " ;
+
+
+tidur 1 ;
+
+
+echo " [+] File: $ file " ;
+
+
+tidur 1 ;
+
+
+echo " [+] Delay: $ delay seconds " ;
+
+
+tidur 2 ;
+
+
+echo " [+] Menjalankan http: // $ server / $ file ... " ;
+
+
+tidur 3 ;
+
+
+sementara [ benar ]
+
+
+melakukan
+
+
+ikal http: // $ server / $ file
+
+
+tidur $ keterlambatan
+
+
+bersih
+
+
+selesai
+
